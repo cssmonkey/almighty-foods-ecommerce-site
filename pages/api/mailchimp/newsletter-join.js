320b12f21d198e709ed133c6dfabdeec2a6b4aa6
Sean Jones <web@sean-jones.co.uk>
2022-01-17 09:24:24 +0000

    Development (#7)
    * design amends and content input
    
    * recipe pages design amends
    
    * working through design amends product page, homepage, recipes
    
    * hero footer etc
    
    * remove response font-sizes, fadein/out hero
    
    * response text sizes
    
    * trying to fix darkmode styles being applied accidentally
    
    * update font sizing & spacing
    
    * increase recipe fontsize

--- pages/api/mailchimp/newsletter-join.js
+++ pages/api/mailchimp/newsletter-join.js
@@ -1,40 +1,42 @@
-import crypto from 'crypto'
-import mailchimp from '@mailchimp/mailchimp_marketing'
+import crypto from 'crypto';
+import mailchimp from '@mailchimp/mailchimp_marketing';
 
 mailchimp.setConfig({
   apiKey: process.env.MAILCHIMP_API_KEY,
   server: process.env.MAILCHIMP_SERVER,
-})
+});
+
+const audienceID = process.env.MAILCHIMP_AUDIENCE_ID;
 
 export default async function send(req, res) {
   if (req.method !== 'POST') {
-    return res.status(404).json({ error: 'must be a POST request' })
+    return res.status(404).json({ error: 'must be a POST request' });
   }
 
   const {
-    body: { audienceID, email },
-  } = req
+    body: { email },
+  } = req;
 
   // honeypot
   if (req.body.fullname !== '') {
-    console.log('stuck in honey')
-    return res.status(200).json({ status: 202 })
+    console.log('stuck in honey');
+    return res.status(200).json({ status: 202 });
   }
 
   if (!email || !audienceID) {
-    console.log('no email or audience ID provided')
+    console.log('no email or audience ID provided');
     return res
       .status(404)
-      .json({ error: 'must contain an email address and audience ID' })
+      .json({ error: 'must contain an email address and audience ID' });
   }
 
-  const subHash = crypto.createHash('md5').update(email).digest('hex')
+  const subHash = crypto.createHash('md5').update(email).digest('hex');
 
   const sendData = await mailchimp.lists.setListMember(audienceID, subHash, {
     email_address: email,
     status_if_new: 'subscribed',
-  })
+  });
 
-  res.statusCode = 200
-  res.json({ sendData })
+  res.statusCode = 200;
+  res.json({ sendData });
 }
